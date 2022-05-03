7240faf7f333b54c364ec981d091c2346b679297
dcz <gihuac.dcz@porcupinefactory.org>
2022-05-03 16:28:55 +0000

    api: Add sleep to alarm

--- libtock2/examples/leds.rs
+++ libtock2/examples/leds.rs
@@ -3,16 +3,20 @@
 #![no_main]
 #![no_std]
 
+use libtock2::alarm::{Alarm, Milliseconds};
 use libtock2::leds::Leds;
 use libtock2::runtime::{set_main, stack_size};
 
 set_main! {main}
-stack_size! {0x100}
+stack_size! {0x200}
 
 fn main() {
     if let Ok(leds_count) = Leds::count() {
-        for led_index in 0..leds_count {
-            let _ = Leds::on(led_index as u32);
+        loop {
+            for led_index in 0..leds_count {
+                let _ = Leds::toggle(led_index as u32);
+            }
+            Alarm::sleep_for(Milliseconds(250)).unwrap();
         }
     }
 }
